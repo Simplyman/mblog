61115f8af79b09f6f85bf11a496cfa626688a7b0
liuziyin <ziyinliu@163.com>
2020-06-10 09:23:29 +0800

    second commit

--- mblog/settings.py
+++ mblog/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'mainsite',
 )
 
 MIDDLEWARE_CLASSES = (
@@ -85,9 +86,13 @@ DATABASES = {
 # Internationalization
 # https://docs.djangoproject.com/en/1.8/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+# LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+# TIME_ZONE = 'UTC'
+
+LANGUAGE_CODE = 'zh-CN'
+
+TIME_ZONE = 'Asia/Shanghai'
 
 USE_I18N = True
 
